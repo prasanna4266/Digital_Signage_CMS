ff140040c04eee2a2814b2a0709ea970afed22bb
prasanna4266 <147905010+prasanna4266@users.noreply.github.com>
2025-08-16 11:44:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,11 +11,12 @@ class Config:
     # For local MongoDB:
     
 
-    MONGO_URI = os.environ.get('MONGO_URI') or "mongodb+srv://prasannabollineni2:pwd@cluster0.ngdtd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    MONGO_DB_NAME = "digital_signage_db" # Name of your database within MongoDB
+    MONGO_URI = os.environ.get('MONGO_URI') 
+    
 
     # If using MongoDB Atlas, your MONGO_URI would look something like this:
     # MONGO_URI = os.environ.get('MONGO_URI') or "mongodb+srv://<username>:<password>@cluster0.abcde.mongodb.net/?retryWrites=true&w=majority"
     # Make sure to replace <username> and <password> with your Atlas database user credentials.
 
     # It's best practice to load this from environment variables.
+
